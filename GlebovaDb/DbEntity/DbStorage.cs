3e131d2f60a9b212629562e7c06478ffb654086a
anastasiaglebova <nglebova04@gmail.com>
2023-03-21 13:19:46 +0300

    pract

---
+++ GlebovaDb/DbEntity/DbStorage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlebovaDb.DbEntity
+{
+   public static class DbStorage
+    {
+      
+    }
+}
+
